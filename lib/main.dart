a261e8d662f829c27b9a7bebac9627fa92c1ab31
Goral <koladiyagoral@gmail.com>
2022-08-05 15:45:04 +0530

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:external_path/external_path.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:image/image.dart' as imglib;
@@ -22,6 +23,8 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
 
   List<Image> list=[];
+  List<Image> tempimglist=[];
+  List temp=List.filled(9, false);
   int horizontalPieceCount=3;
   int verticalPieceCount=3;
   List<Image> splitImage(List<int> input)
@@ -45,7 +48,7 @@ class _MyAppState extends State<MyApp> {
     // convert image from image package to Image Widget to display
     List<Image> output = [];
     for (var img in parts) {
-      output.add(Image.memory(Uint8List.fromList(imglib.encodeJpg(img))));
+      output.add(Image.memory(Uint8List.fromList(imglib.encodeJpg(img)),fit: BoxFit.fill,));
     }
 
     return output;
@@ -67,6 +70,8 @@ class _MyAppState extends State<MyApp> {
       List<int> data=value.readAsBytesSync();
       setState(() {
         list=splitImage(data);
+        tempimglist.addAll(list);
+        list.shuffle();
       });
       print(list);
     });
@@ -77,10 +82,44 @@ class _MyAppState extends State<MyApp> {
     return Scaffold(
       appBar: AppBar(title: Text("APP"),),
       body: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3,mainAxisSpacing: 5,crossAxisSpacing: 5),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3,mainAxisSpacing: 1,crossAxisSpacing: 1),
         itemCount:list.length,
         itemBuilder: (context, index) {
-        return GridTile(child: list[index]);
+        return temp[index]?DragTarget(onAccept:(data) {
+          print(data);
+          setState(() {
+            int t=data as int;
+            Image w=list[t];
+            list[t]=list[index];
+            list[index]=w;
+          });
+          if(listEquals(tempimglist, list))
+            {
+              print("Success");
+            }
+          else
+            {
+              print("not");
+            }
+        },builder: (context, candidateData, rejectedData) {
+          return Container(child: list[index]);
+        },onWillAccept: (data) {
+          return true;
+        },
+        ):
+        Draggable(onDragEnd:(details) {
+          temp=List.filled(9, false);
+        },onDragStarted: (){
+          setState(() {
+            for(int i=0;i<9;i++)
+              {
+                if(i!=index)
+                  {
+                    temp[i]=true;
+                  }
+              }
+          });
+        },data:index,child: Container(child: list[index]),feedback: Container(child: list[index]),childWhenDragging: Container(),);
       },),
     );
   }
